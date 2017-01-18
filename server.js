c85b81005ce60c41118334fb3a9eaca45f103ead
janemrivera <jane.m.rivera@gmail.com>
2017-01-18 16:14:45 -0600

    appexpress

--- server.js
+++ server.js
@@ -1,75 +1,14 @@
-'use strict';
 
-var Flint = require('node-flint');
-var webhook = require('node-flint/webhook');
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var Imgflipper = require('imgflipper');
+var express = require('express')
+var app = express()
 
-var http = require('http');
+app.get('/', function (req, res) {
+  res.send('Hello World Root!')
+})
 
-var app = express();
-// Setup the View Engine
-//app.set("view engine", "jade");
-//app.engine("ejs", ejsEngine); // support master pages
-//app.set("view engine", "ejs"); // ejs view engine
-//app.set("view engine", "vash");
-
-// Opt into Services
-//app.use(express.urlencoded());
-//app.use(express.cookieParser());
-//app.use(express.session({ secret: "PluralsightTheBoard" }));
-//app.use(flash());
-
-// set the public static resource folder
-app.use(express.static(__dirname + "/public"));
-
-app.use(bodyParser.json());
-//const config = require("./config.json");
-
-var config = {
-  "webhookUrl": process.env.WEBHOOKURL,
-  "token": process.env.BOTTOKEN,
-  "port": process.env.PORT
-}
-
-
-/****
-## Server config & housekeeping
-****/
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
-
-app.get('/health', function(req, res){
-    res.send('OK');
-});
-
-app.get("/api/users", function (req, res) {
-  res.set("Content-Type", "application/json");
-  res.send({ name: "Jane", isValid: true, group: "Admin" });
-});
-
-// define express path for incoming webhooks
-//app.post('/', webhook(flint));
-
-// start express server
-var server = app.listen(8080, function () {
-  console.log('Flint listening on port %s', "8080");
-});
-
-// gracefully shutdown (ctrl-c)
-process.on('SIGINT', function() {
-  //flint.debug('stopping...');
-  server.close();
-
-  //flint.stop().then(function() {
-  //  process.exit();
-  //});
-
-});
-
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
 
 
 
