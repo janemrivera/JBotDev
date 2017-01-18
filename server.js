c02f24dd78f9b83e44ca754c7e57563eb8e36098
janemrivera <jane.m.rivera@gmail.com>
2017-01-18 11:39:50 -0600

    basic

--- server.js
+++ server.js
@@ -1,23 +1,5 @@
 var http = require('http');
 
-/****
-## Server config & housekeeping
-****/
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
-
-app.get('/health', function(req, res){
-    res.send('OK');
-});
-
-app.get("/api/users", function (req, res) {
-  res.set("Content-Type", "application/json");
-  res.send({ name: "Jane", isValid: true, group: "Admin" });
-});
-
-
-
 http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
